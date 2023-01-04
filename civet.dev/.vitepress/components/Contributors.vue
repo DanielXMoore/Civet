6acdc1a7f1d491c573eff2ffa270300af3dc3408
Adrian Wieprzkowicz <Argeento@users.noreply.github.com>
2023-01-05 00:32:49 +0100

    Update docs (#115)

---
+++ civet.dev/.vitepress/components/Contributors.vue
@@ -0,0 +1,53 @@
+<script setup>
+import { useData } from 'vitepress';
+
+const users = useData().theme.value.contributors;
+</script>
+
+<template>
+  <ul class="items">
+    <li v-for="user in users" :key="user.id" class="item">
+      <a :href="user.href" target="_blank" class="user">
+        <img :src="user.avatar" :alt="user.login" class="img" />
+        <span>
+          {{ user.login }}
+          <span class="commits">({{ user.contributions }})</span>
+        </span>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<style scoped>
+.items {
+  list-style: none;
+  margin: 10px 0;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 12px 10px;
+}
+
+.item {
+  margin: 0;
+}
+
+.user {
+  display: flex;
+  align-items: center;
+  background: var(--vp-c-bg-mute);
+  border-radius: 5px;
+  padding: 10px;
+}
+
+.img {
+  width: 30px;
+  border-radius: 50%;
+  margin-right: 10px;
+  display: inline-block;
+}
+
+.commits {
+  color: var(--vp-c-text-2);
+}
+</style>
---
+++ civet.dev/.vitepress/components/Hero.vue
@@ -0,0 +1,43 @@
+<script setup>
+import VPHero from 'vitepress/dist/client/theme-default/components/VPHero.vue';
+
+const actions = [
+  {
+    theme: 'brand',
+    text: 'Cheatsheet',
+    link: '/cheatsheet',
+  },
+  {
+    theme: 'alt',
+    text: 'Civet playground',
+    link: 'https://civet-web.vercel.app/',
+  },
+];
+</script>
+
+<template>
+  <VPHero
+    class="hero"
+    name="Civet"
+    text="The Modern Way to Write TypeScript"
+    tagline="Concise Syntax and Faster Coding with Civet"
+    :image="{
+      src: 'https://user-images.githubusercontent.com/13007891/210392977-03a3b140-ec63-4ce9-b6e3-0a0f7cac6cbe.png',
+      alt: 'civet',
+    }"
+    :actions="actions"
+  />
+</template>
+
+<style scoped>
+@media (min-width: 768px) {
+  .VPHero {
+    padding-left: 0 !important;
+    padding-right: 0 !important;
+  }
+  .VPHero:deep(.VPImage) {
+    margin-top: 30px;
+    margin-left: 50px;
+  }
+}
+</style>
