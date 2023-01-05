3e9d071e712c3bf5f695dcb806556315f72c5e27
Daniel Moore <daniel@danielx.net>
2023-01-04 18:17:57 -0800

    using github token to authorize api request

--- civet.dev/.vitepress/utils.js
+++ civet.dev/.vitepress/utils.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
 export function getContributors() {
+  const headers = {
+    'Authorization': `Bearer ${process.env.GITHUB_TOKEN}`
+  };
+
   return axios
-    .get('https://api.github.com/repos/DanielXMoore/Civet/contributors')
+    .get('https://api.github.com/repos/DanielXMoore/Civet/contributors', {
+      headers
+    })
     .then((res) => {
       return res.data
         .filter((user) => user.type.toLowerCase() === 'user')
