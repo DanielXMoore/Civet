f31731f498a717ac626beebdc5aba0efb9c4024d
Adrian Wieprzkowicz <adrian.wieprzkowicz@gmail.com>
2023-01-07 02:37:13 +0100

    Docs: fix code examples rendering

--- civet.dev/.vitepress/config.js
+++ civet.dev/.vitepress/config.js
@@ -1,9 +1,14 @@
 import pkg from '../../package.json';
 import { head } from './head.js';
-import { getContributors } from './utils.js';
+import { getContributors } from './utils/getContributors.js';
 import { defineConfig } from 'vitepress';
+import { compileCivet } from './utils/compileCivet';
+import { getHighlighter } from './utils/getHighlighter';
+import civet from '../../dist/main.mjs';
+import prettier from 'prettier';
 
 export default async function vitePressConfig() {
+  const highlighter = await getHighlighter();
   return defineConfig({
     lang: 'en-US',
     title: 'Civet - The Modern Way to Write TypeScript',
@@ -51,8 +56,15 @@ export default async function vitePressConfig() {
 
           if (token.content.startsWith('<Playground')) {
             const lines = token.content.trim().split('\n');
-            const code = encodeURI(lines.slice(1, -1).join('\n'));
-            return lines[0] + code + lines[lines.length - 1];
+            const code = lines.slice(1, -1).join('\n');
+            const { tsCode } = compileCivet(code, civet, prettier);
+            const inputHtml = highlighter.codeToHtml(code, { lang: 'coffee' });
+            const outputHtml = highlighter.codeToHtml(tsCode, { lang: 'tsx' });
+
+            return `${lines[0]}
+              <template #input>${inputHtml}</template>
+              <template #output>${outputHtml}</template>
+            ${lines[lines.length - 1]}`;
           }
 
           return defaultRender(tokens, idx, options, env, self);
---
+++ civet.dev/.vitepress/utils/compileCivet.ts
@@ -0,0 +1,23 @@
+// TODO: Fix "any"
+export function compileCivet(
+  code: string,
+  civetInstance: any,
+  prettierInstance: any
+) {
+  let tsCode = civetInstance.compile(code);
+  const tsRawCode = tsCode;
+
+  try {
+    tsCode = prettierInstance.format(tsCode, {
+      parser: 'typescript',
+      printWidth: 50,
+    });
+  } catch (err) {
+    console.info('Prettier error. Fallback to raw civet output', {
+      tsCode,
+      err,
+    });
+  }
+
+  return { tsCode, tsRawCode };
+}
