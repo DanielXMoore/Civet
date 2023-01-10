902a7a4aa18064903b5e01a54e7b9b7e367ac9eb
Adrian Wieprzkowicz <Argeento@users.noreply.github.com>
2023-01-10 04:25:52 +0100

    Docs: add interactive snippets (#172)
    * Docs: add sponsors
    
    * Docs: add interactive snippets

--- civet.dev/public/playground.worker.js
+++ civet.dev/public/playground.worker.js
@@ -1,30 +1,48 @@
 importScripts('https://unpkg.com/prettier@2.8.1/standalone.js');
 importScripts('https://unpkg.com/prettier@2.8.1/parser-typescript.js');
+importScripts('https://unpkg.com/shiki');
 importScripts('/__civet.js');
 
-onmessage = (e) => {
-  const { uid, code } = e.data;
+onmessage = async (e) => {
+  const { uid, code, prettierOutput } = e.data;
+  const highlighter = await getHighlighter();
+  const inputHtml = highlighter.codeToHtml(code, { lang: 'coffee' });
 
   try {
-    const tsCode = Civet.compile(code);
-    let prettierCode = '';
+    let tsCode = Civet.compile(code);
 
-    try {
-      prettierCode = prettier.format(tsCode, {
-        parser: 'typescript',
-        plugins: prettierPlugins,
-        printWidth: 50,
-      });
-    } catch (err) {
-      prettierCode = tsCode;
-      console.info('Prettier error. Fallback to raw civet output', {
-        tsCode,
-        err,
-      });
+    if (prettierOutput) {
+      try {
+        tsCode = prettier.format(tsCode, {
+          parser: 'typescript',
+          plugins: prettierPlugins,
+          printWidth: 50,
+        });
+      } catch (err) {
+        console.info('Prettier error. Fallback to raw civet output', {
+          tsCode,
+          err,
+        });
+      }
     }
 
-    postMessage({ uid, tsCode, prettierCode });
-  } catch (err) {
-    postMessage({ uid, err });
+    const outputHtml = highlighter.codeToHtml(tsCode, { lang: 'tsx' });
+
+    postMessage({ uid, inputHtml, outputHtml });
+  } catch (error) {
+    console.info('Snippet compilation error!', error);
+    postMessage({ uid, inputHtml, error });
   }
 };
+
+let highlighter;
+async function getHighlighter() {
+  if (!highlighter) {
+    highlighter = await shiki.getHighlighter({
+      theme: 'one-dark-pro',
+      langs: ['coffee', 'tsx'],
+    });
+  }
+
+  return highlighter;
+}
