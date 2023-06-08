fdd089e2f1f25bd7fab230d1596c909573d1e377
Daniel Moore <daniel@danielx.net>
2023-06-08 15:22:28 -0700

    options to babel plugin

--- source/babel-plugin.mjs
+++ source/babel-plugin.mjs
@@ -1,9 +1,23 @@
 import { compile } from "./main.mjs"
 
-export default function (api) {
+export default function (api, civetOptions) {
   return {
     parserOverride(code, opts, parse) {
-      const src = opts.sourceFileName.endsWith(".civet") ? compile(code) : code
+      let src
+      if (opts.sourceFileName.endsWith(".civet")) {
+        const config = Object.assign({}, civetOptions, {
+          filename: opts.sourceFileName,
+          sourceMap: opts.sourceMaps ?? true,
+        })
+
+        if (config.sourceMap) {
+          ({ code: src } = compile(code, config))
+        } else {
+          src = compile(code, config)
+        }
+      } else {
+        src = code
+      }
       const ast = parse(src, opts)
       return ast
     }
