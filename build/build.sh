eedee6ee05051ef3916bf2482994278794b7f0ee
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-08-16 18:43:32 +0530

    move unplugin deps to top level

--- build/build.sh
+++ build/build.sh
@@ -20,7 +20,6 @@ rm dist/cli.js
 cp types/types.d.ts dist/types.d.ts
 
 # unplugin
-yarn --cwd ./integration/unplugin install
 yarn --cwd ./integration/unplugin build
 cp ./integration/unplugin/dist/* ./dist
 
