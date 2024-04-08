bbb2addc71cf55bbcc8e7a7792935129ab27c180
Erik Demaine <edemaine@mit.edu>
2024-04-08 12:02:25 -0400

    Extract DEFAULT_EXTENSIONS from Vite

--- integration/unplugin/src/index.ts
+++ integration/unplugin/src/index.ts
@@ -14,6 +14,7 @@ import * as tsvfs from '@typescript/vfs';
 import type { UserConfig } from 'vite';
 import type { BuildOptions } from 'esbuild';
 import os from 'os';
+import { DEFAULT_EXTENSIONS } from './constants.js';
 
 // Copied from typescript to avoid importing the whole package
 enum DiagnosticCategory {
@@ -458,11 +459,7 @@ export const rawPlugin: Parameters<typeof createUnplugin<PluginOptions>>[0] =
 
         if (implicitExtension) {
           config.resolve ??= {};
-          config.resolve.extensions ??= [
-            // Vite's DEFAULT_EXTENSIONs from
-            // https://github.com/vitejs/vite/blob/main/packages/vite/src/node/constants.ts#L29
-            '.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json',
-          ];
+          config.resolve.extensions ??= DEFAULT_EXTENSIONS;
           config.resolve.extensions.push('.civet');
         }
       },
