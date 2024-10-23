a365833803e193f46b8552692e0955cc09bfd76c
Erik Demaine <edemaine@mit.edu>
2024-10-23 16:18:24 -0400

    Fix tsconfig.json naming issue

--- lsp/build/build.sh
+++ lsp/build/build.sh
@@ -9,4 +9,4 @@ node_modules/.bin/civet build/build.civet
 
 mkdir -p dist/lib
 cp node_modules/typescript/lib/lib.*.d.ts dist/lib
-cp source/tsconfig.json dist/lib
+cp source/tsconfig-lib.json dist/lib/tsconfig.json
