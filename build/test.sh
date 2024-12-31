27619ea4bbf34f727408b2c8af3b96e16c1da845
Erik Demaine <edemaine@mit.edu>
2024-12-30 20:19:31 -0500

    Fix CIVET_THREADS check

--- build/test.sh
+++ build/test.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 # Translate CIVET_THREADS into Mocha's --parallel
 # In particular, CIVET_THREADS Workers don't work within Mocha
 args=""
-if [ ! -z "${CIVET_THREADS:-}" -a "$CIVET_THREADS" != 0 ]; then
+if [ "${CIVET_THREADS:-0}" != 0 ]; then
   args="--parallel -j $CIVET_THREADS"
   export CIVET_THREADS=
 fi
